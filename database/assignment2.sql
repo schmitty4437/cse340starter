91fb8a470cb95f9d1f46def6860f64d970e8e8b5
schmitty4437 <schmitty4437@gmail.com>
2025-01-25 18:49:24 -0700

    assignment 2 fixes

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -4,17 +4,17 @@ INSERT INTO public.account (
         account_email,
         account_password
     )
-VALUES ('Tony'),
-    ('Stark'),
-    ('tony@startknet.com'),
-    ('Iam1ronM@n');
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starknet.com',
+        'Iam1ronM@n'
+    );
 UPDATE public.account
 SET account_type = 'Admin'
-WHERE id = 1;
+WHERE account_id = 1;
 DELETE FROM public.account
-WHERE id = 1;
-SELECT *
-FROM public.account;
+WHERE account_id = 1;
 UPDATE public.inventory
 SET inv_description = REPLACE(
         inv_description,
@@ -22,17 +22,14 @@ SET inv_description = REPLACE(
         'a huge interior'
     )
 WHERE inv_make = 'GM';
-SELECT *
-FROM public.inventory;
 SELECT inv_make,
     inv_model,
     classification_name
 FROM public.inventory pi
-    INNER JOIN public.classification pc ON pi.classification_id = pc.classification_id;
+    INNER JOIN public.classification pc ON pi.classification_id = pc.classification_id
+WHERE classification_name = 'Sport';
 UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
 WHERE inv_image LIKE '/images/%'
     OR inv_thumbnail LIKE '/images/%';
-SELECT *
-FROM public.inventory;
